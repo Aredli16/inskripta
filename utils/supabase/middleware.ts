68e0694da95b95e7a81508b9846cd8df9d59940b
corentin <xtarak@gmail.com>
2025-04-08 14:02:53 +0200

    #12: Redirection après la connexion

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -43,6 +43,10 @@ export async function updateSession(request: NextRequest) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/auth";
+    url.searchParams.set(
+      "redirectTo",
+      request.nextUrl.pathname + request.nextUrl.search,
+    );
     return NextResponse.redirect(url);
   }
 
