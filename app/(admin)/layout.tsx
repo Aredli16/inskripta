adb85849410412336f33345b5ec7423118ab3d40
corentin <xtarak@gmail.com>
2025-04-08 13:38:07 +0200

    #10: Mettre en place le layout admin

---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,17 @@
+import "../globals.css";
+import { ReactNode } from "react";
+import { getLocale } from "next-intl/server";
+import { NextIntlClientProvider } from "next-intl";
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+  const locale = await getLocale();
+
+  return (
+    <html lang={locale} className="h-full bg-gray-100">
+      <body className="h-full">
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  );
+};
+export default Layout;
