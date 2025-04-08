a520e63eda30df0c92badb33d48bd06dd4fed43e
corentin <xtarak@gmail.com>
2025-04-08 10:28:41 +0200

    #5: Ajouter Prettier

--- app/auth/actions.ts
+++ app/auth/actions.ts
@@ -1,62 +1,67 @@
-'use server'
+"use server";
 
-import {revalidatePath} from 'next/cache'
-import {redirect} from 'next/navigation'
-import {createClient} from '@/utils/supabase/server'
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
 
 export type AuthState = {
-    error?: string
+  error?: string;
+};
+
+export async function login(
+  previousState: AuthState,
+  formData: FormData,
+): Promise<AuthState> {
+  const supabase = await createClient();
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  const { error } = await supabase.auth.signInWithPassword(data);
+
+  if (error) {
+    return {
+      error: error.code,
+    };
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/");
 }
 
-export async function login(previousState: AuthState, formData: FormData): Promise<AuthState> {
-    const supabase = await createClient()
+export async function register(
+  previousState: AuthState,
+  formData: FormData,
+): Promise<AuthState> {
+  const supabase = await createClient();
+  const password = formData.get("password") as string;
+  const confirmPassword = formData.get("confirmPassword") as string;
 
-    // type-casting here for convenience
-    // in practice, you should validate your inputs
-    const data = {
-        email: formData.get('email') as string,
-        password: formData.get('password') as string,
-    }
+  if (password !== confirmPassword) {
+    return {
+      error: "passwords do not match",
+    };
+  }
 
-    const {error} = await supabase.auth.signInWithPassword(data)
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get("email") as string,
+    password: password,
+  };
 
-    if (error) {
-        return {
-            error: error.code
-        }
-    }
+  const { error } = await supabase.auth.signUp(data);
 
-    revalidatePath('/', 'layout')
-    redirect('/')
-}
+  if (error) {
+    return {
+      error: error.code,
+    };
+  }
 
-export async function register(previousState: AuthState, formData: FormData): Promise<AuthState> {
-    const supabase = await createClient()
-    const password = formData.get('password') as string
-    const confirmPassword = formData.get('confirmPassword') as string
-
-
-    if (password !== confirmPassword) {
-        return {
-            error: 'passwords do not match'
-        }
-    }
-
-    // type-casting here for convenience
-    // in practice, you should validate your inputs
-    const data = {
-        email: formData.get('email') as string,
-        password: password,
-    }
-
-    const {error} = await supabase.auth.signUp(data)
-
-    if (error) {
-        return {
-            error: error.code
-        }
-    }
-
-    revalidatePath('/', 'layout')
-    redirect('/')
+  revalidatePath("/", "layout");
+  redirect("/");
 }
--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,16 +1,18 @@
-import {createClient} from "@/utils/supabase/server";
-import {redirect} from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 import LoginForm from "@/components/form/LoginForm";
 
 const Page = async () => {
-    const supabase = await createClient();
-    const {data: {user}} = await supabase.auth.getUser();
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-    if (user) {
-        redirect("/")
-    }
+  if (user) {
+    redirect("/");
+  }
 
-    return <LoginForm/>;
+  return <LoginForm />;
 };
 
 export default Page;
