d80a9c68d63ee1b968dad8fb26da0adfdb835aaa
Lempereur Corentin <73390812+aredli16@users.noreply.github.com>
2025-04-19 16:43:20 +0200

    #20: Parcours utilisateur pour les inscriptions

--- app/[domain]/(home)/page.tsx
+++ app/[domain]/(home)/page.tsx
@@ -8,16 +8,20 @@ import {
   MenuItems,
 } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useTranslations } from "next-intl";
 import Link from "next/link";
 import Image from "next/image";
 import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { CheckCircleIcon, ChevronDownIcon } from "@heroicons/react/20/solid";
+import { getTranslations } from "next-intl/server";
 
-const Page = () => {
-  const t = useTranslations("Domain.Home");
+const Page = async () => {
+  const t = await getTranslations("Domain.Home");
+  const supabase = await createClient();
+  const { data: registrations } = await supabase
+    .from("registrations")
+    .select("*, students(*)");
 
   return (
     <>
@@ -120,34 +124,110 @@ const Page = () => {
           </DisclosurePanel>
         </Disclosure>
       </header>
-      <main className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <Menu as="div" className="relative inline-block text-left">
-          <div>
-            <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50">
-              {t("Registration.Create.Title")}
-              <ChevronDownIcon
-                aria-hidden="true"
-                className="-mr-1 size-5 text-gray-400"
-              />
-            </MenuButton>
-          </div>
-
-          <MenuItems
-            transition
-            className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-          >
-            <div className="py-1">
-              <MenuItem>
-                <Link
-                  href="/register"
-                  className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
-                >
-                  {t("Registration.Create.NewStudent")}
-                </Link>
-              </MenuItem>
+      <main className="mx-auto max-w-7xl sm:px-2 lg:px-8 mt-16">
+        <div className="mx-auto max-w-2xl space-y-8 sm:px-4 lg:max-w-4xl lg:px-0">
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50">
+                {t("Registration.Create.Title")}
+                <ChevronDownIcon
+                  aria-hidden="true"
+                  className="-mr-1 size-5 text-gray-400"
+                />
+              </MenuButton>
             </div>
-          </MenuItems>
-        </Menu>
+
+            <MenuItems
+              transition
+              className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+            >
+              <div className="py-1">
+                <MenuItem>
+                  <Link
+                    href="/register"
+                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                  >
+                    {t("Registration.Create.NewStudent")}
+                  </Link>
+                </MenuItem>
+              </div>
+            </MenuItems>
+          </Menu>
+        </div>
+
+        <div className="mt-16">
+          <h2 className="sr-only">{t("Registration.List.Title")}</h2>
+          <div className="mx-auto max-w-7xl sm:px-2 lg:px-8">
+            <div className="mx-auto max-w-2xl space-y-8 sm:px-4 lg:max-w-4xl lg:px-0">
+              {registrations.map((registration) => (
+                <div
+                  key={registration.id}
+                  className="border-t border-b border-gray-200 bg-white shadow-xs sm:rounded-lg sm:border"
+                >
+                  <h3 className="sr-only">
+                    {t("Registration.List.CreatedAt")}{" "}
+                    <time dateTime={registration.created_at}>
+                      {registration.created_at}
+                    </time>
+                  </h3>
+
+                  <div className="flex items-center border-b border-gray-200 p-4 sm:grid sm:grid-cols-4 sm:gap-x-6 sm:p-6">
+                    <dl className="grid flex-1 grid-cols-2 gap-x-6 text-sm sm:col-span-3 sm:grid-cols-3 lg:col-span-2">
+                      <div>
+                        <dt className="font-medium text-gray-900">
+                          {t("Registration.List.ID")}
+                        </dt>
+                        <dd className="mt-1 text-gray-500">
+                          {registration.id}
+                        </dd>
+                      </div>
+                      <div className="hidden sm:block">
+                        <dt className="font-medium text-gray-900">
+                          {t("Registration.List.CreatedAt")}
+                        </dt>
+                        <dd className="mt-1 text-gray-500">
+                          <time dateTime={registration.created_at}>
+                            {new Date(registration.created_at).toLocaleString()}
+                          </time>
+                        </dd>
+                      </div>
+                    </dl>
+                  </div>
+
+                  <ul role="list" className="divide-y divide-gray-200">
+                    <li className="p-4 sm:p-6">
+                      <div className="flex items-center sm:items-start">
+                        <div className="flex-1 text-sm">
+                          <div className="font-medium text-gray-900 sm:flex sm:justify-between">
+                            <h5>
+                              {registration.students.last_name}{" "}
+                              {registration.students.first_name}
+                            </h5>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="mt-6 sm:flex sm:justify-between">
+                        <div className="flex items-center">
+                          <CheckCircleIcon
+                            aria-hidden="true"
+                            className="size-5 text-green-500"
+                          />
+                          <p className="ml-2 text-sm font-medium text-gray-500">
+                            Delivered on{" "}
+                            <time dateTime={registration.created_at}>
+                              {registration.created_at}
+                            </time>
+                          </p>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
